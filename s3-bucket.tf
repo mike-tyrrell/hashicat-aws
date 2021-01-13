3a1eb03bb0aad9fea5509a14dea2a1b035bf4b4e
mike-tyrrell <michael.tyrr@gmail.com>
2021-01-13 19:18:39 +0000

    Added s3 bucket module3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,9 @@ module "s3-bucket" {
   source  = "app.terraform.io/mtyrrell-training/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-  acceleration_status = "fast"
-  bucket = "S3"
+  acceleration_status = "Enabled"
+  bucket = "mike-S3"
   bucket_prefix = "mike"
-  policy = "ok"
-  request_payer = "notime"
+  policy = "verbose/specific"
+  request_payer = "BucketOwner"
 }

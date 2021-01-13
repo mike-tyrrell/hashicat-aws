9b730d64008785dce159f22453925e6624262d98
mike-tyrrell <michael.tyrr@gmail.com>
2021-01-13 19:20:48 +0000

    Added s3 bucket module3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,7 @@ module "s3-bucket" {
   version = "1.17.0"
   # insert required variables here
   acceleration_status = "Enabled"
-  bucket = "mike-S3"
+  bucket = mike-S3
   bucket_prefix = "mike"
   policy = "verbose/specific"
   request_payer = "BucketOwner"

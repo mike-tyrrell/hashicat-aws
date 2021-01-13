2c2c5abdf852f292b30228e0cd90c4643f0c9b4f
mike-tyrrell <michael.tyrr@gmail.com>
2021-01-13 19:11:40 +0000

    Added s3 bucket module3

--- main.tf
+++ main.tf
@@ -125,9 +125,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = devops
-    Billable = true
-    crap = dontwork
+    Department = "devops"
+    Billable = "true"
   }
 }
 
